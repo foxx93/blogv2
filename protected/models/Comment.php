21c47fddbc699b007578661c43b86d7872ddfb4d
foxx93 <sofalvigergo@gmail.com>
2015-11-26 15:10:21 +0100

    Comment model modosítások:     - Rules class modosítás (itt szintén hibás a tutorialba a legelső sor, ahol a kötelező sorok bekérése van felsorolva, bele kellet irnom a "post_id"-t.     -  attributeLabels() modsítás: Itt csak a megjelenő szöveget modosítjuk komment írásánál.     - Mentési procedura modosítása: a beforesave modosítása, uj koment írásakor a creation date dátumát az éppen jelenlegi dátummal feltöltjuk adatbázisba, igy lehet tudni mikor lett írva a komment.

--- protected/models/Comment.php
+++ protected/models/Comment.php
@@ -33,17 +33,14 @@ class Comment extends CActiveRecord
 	/**
 	 * @return array validation rules for model attributes.
 	 */
+
 	public function rules()
 	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
 		return array(
-			array('content, status, author, email, post_id', 'required'),
-			array('status, create_time, post_id', 'numerical', 'integerOnly'=>true),
-			array('author, email, url', 'length', 'max'=>128),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, content, status, create_time, author, email, url, post_id', 'safe', 'on'=>'search'),
+				array('content, author, email, post_id', 'required'),
+				array('author, email, url', 'length', 'max'=>128),
+				array('email','email'),
+				array('url','url'),
 		);
 	}
 
@@ -65,14 +62,14 @@ class Comment extends CActiveRecord
 	public function attributeLabels()
 	{
 		return array(
-			'id' => 'ID',
-			'content' => 'Content',
-			'status' => 'Status',
-			'create_time' => 'Create Time',
-			'author' => 'Author',
-			'email' => 'Email',
-			'url' => 'Url',
-			'post_id' => 'Post',
+				'id' => 'Id',
+				'content' => 'Comment',
+				'status' => 'Status',
+				'create_time' => 'Create Time',
+				'author' => 'Name',
+				'email' => 'Email',
+				'url' => 'Website',
+				'post_id' => 'Post',
 		);
 	}
 
@@ -118,4 +115,17 @@ class Comment extends CActiveRecord
 	{
 		return parent::model($className);
 	}
+
+	protected function beforeSave()
+	{
+		if(parent::beforeSave())
+		{
+			if($this->isNewRecord)
+				$this->create_time=time();
+			return true;
+		}
+		else
+			return false;
+	}
+
 }
